25dc1b5048b16167e0172effbe6ed2e01469270c
Tracy Pearson <tracy@tracypearson.com>
2024-03-31 01:08:13 -0400

    I broke it. Overriding the GetHashCode of the model breaks the binding.

--- WinformsMVPMDI/Models/Person.cs
+++ WinformsMVPMDI/Models/Person.cs
@@ -7,10 +7,27 @@ using WinformsMVPMDI.Structure;
 
 namespace WinformsMVPMDI.Models
 {
-    public class Person : ObservableObject
+    public class Person : ObservableObject, IEquatable<Person?>
     {
         public int PersonId { get; set; }
         public string FirstName { get => _firstName; set => Set(ref _firstName, value); }
         private string _firstName = string.Empty;
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Person);
+        }
+
+        public bool Equals(Person? other)
+        {
+            return other is not null &&
+                   PersonId == other.PersonId &&
+                   FirstName == other.FirstName;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PersonId, FirstName);
+        }
     }
 }
